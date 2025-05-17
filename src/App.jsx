8466d346dace9aaa90d32dab3b5d8760af5d72fd
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2025-05-17 18:09:02 +0530

    Add react-router-dom for routing and update App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import Header from './components/layout/Header';
+import Features from './pages/features';
+
 
 const App = () => {
   return <Layout />;
