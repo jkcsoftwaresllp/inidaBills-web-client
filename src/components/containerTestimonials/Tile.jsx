6841a4ab22a25882450a4a670e59dad05bcaaaae
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2025-05-20 10:00:26 +0530

    Refactor Tile component: remove unused props and simplify structure

--- src/components/containerTestimonials/Tile.jsx
+++ src/components/containerTestimonials/Tile.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
 import styles from './styles/Tile.module.css'
-import PasteSVG from '../home/svg/pasteSVG'
-
-
 
 /**
  * Tile Component
@@ -15,23 +12,20 @@ import PasteSVG from '../home/svg/pasteSVG'
  * @param {Object} props - Props passed to the component.
  * @param {string} props.tittle - The title or heading displayed at the top of the tile.
  * @param {string} props.para - The paragraph or description content of the tile.
- * @param {string} props.icon - Icon of Tile
- * @param {string} props.width - Width of Tile only 28vw and 45vw
  *
  * @returns {JSX.Element} The rendered Tile component.
  */
-function Tile({ tittle, para, icon, width, iconDirection }) { // only---  width="28vw" , width="45vw" & iconDirection="inline" for inline , Null for Block
-  return (
-    <div className={`${styles.box} ${iconDirection === "inline" ? styles.iconInlineBox : styles.iconBlockBox}`} style={{ width }} >
-      <div className={`${styles.icon} ${iconDirection === "inline" ? styles.iconInline : styles.iconBlock}`} >
-        {icon}
-      </div>
-      <div className={`${iconDirection === "inline" ? styles.whenTextInline : ""}`} >
-        <h1 className={styles.tittle} >{tittle}</h1>
-        <h4 className={styles.para} >{para}</h4>
-      </div>
-    </div>
-  )
+function Tile({ tittle, para }) {
+    return (
+        <div className={styles.box}>
+          <h1 className={styles.tittle}>{tittle}</h1>
+          <h4 className={styles.para}>
+        {Array.isArray(para)
+          ? para.map((line, idx) => <React.Fragment key={idx}>{line}<br/></React.Fragment>)
+          : para}
+      </h4>
+        </div>
+    )
 }
 
 export default Tile
